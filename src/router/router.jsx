827b69359ff7d329f217b2c5207c649c0d5bf61d
razaulkarim2410 <razaulkarim2410@gmail.com>
2025-06-10 01:10:49 +0600

    create navbar section

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,6 +3,8 @@ import {
 } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
+import LogIn from "../pages/logIn/LogIn";
+import Service from "../pages/Service/Service";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,14 @@ const router = createBrowserRouter([
       {
         index: true,
         Component: Home,
+      },
+      {
+        path: '/service',
+        Component: Service,
+      },
+      {
+        path:'/login',
+        Component: LogIn,
       }
     ]
   },
